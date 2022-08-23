7c46f1e9d77c3aee78f4650992c70d0e6fab3cd2
kate <e.nikitina@dunice.net>
2022-08-23 16:36:35 +0300

    Deleted commented

--- nikitina_backend/nikitina_backend/settings.py
+++ nikitina_backend/nikitina_backend/settings.py
@@ -111,9 +111,6 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
    'rest_framework.permissions.AllowAny',
 ],
-    # 'DEFAULT_PARSER_CLASSES': [
-    #     'rest_framework.parsers.JSONParser',
-    # ]
 }
 
 SIMPLE_JWT = {
